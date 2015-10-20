12992fa24ab0961f60cd1a546663e5f2c956d606
Wang Biwen <wangbiwen88@126.com>
2015-10-20 16:25:29 +0800

    edit 16_3Sum_Closest.py

--- python/16_3Sum_Closest.py
+++ python/16_3Sum_Closest.py
@@ -13,3 +13,22 @@ class Solution:
 	# @return {integer}
 	def threeSumClosest(self, nums, target):
 		nums.sort()
+		for k in range(len(nums)):
+			i = k + 1
+			j = len(nums) - 1
+			tmp = target - nums[k]
+			pre = None
+			if (nums[i] + nums[j] - tmp) * (pre - tmp) <= 0:
+				abs1 = abs(nums[i] + nums[j] - tmp)
+				abs2 = abs(pre - tmp)
+				if abs1 = min(abs1, abs2):
+					resultList.append
+
+
+
+
+solution = Solution()
+s = [-1,2,1,-4]
+target = 1
+result = solution.threeSumClosest(s, target)
+print result
