912bb23b8a305c3e03679f6d4b0f1f9b3d26be97
biwen <wangbiwen_88@qq.com>
2016-04-22 13:05:10 +0800

    update the solution for the 1st problem.

--- python/1_Two_Sum.py
+++ python/1_Two_Sum.py
@@ -7,39 +7,31 @@
 #这是我最初的解法思想，类似于快排的原理，就是两个指针从首尾分别向中间移动查找。需要先进行排序。
 #
 class Solution:
-	#@param {integer[]} nums
-	#@param {integer} target
-	#@return {integer[]} 
-	def twoSum(self, nums, target):
-		dic = {} #用来存储原始数组中元素与下标的映射关系，有可能一个元素对应多个下标
-		for i in range(len(nums)):
-			if dic.has_key(nums[i]):
-				dic[nums[i]].append(i)
-			else:
-				dic[nums[i]] = []
-				dic[nums[i]].append(i)
+    #@param {integer[]} nums
+    #@param {integer} target
+    #@return {integer[]} 
+    def twoSum(self, nums, target):
+        dic = {} #用来存储原始数组中元素与下标的映射关系，有可能一个元素对应多个下标
+        for i, element in enumerate(nums):
+            lst = dic.get(nums[i], [])
+            lst.append(i)
+            dic[nums[i]] = lst
 
-		nums.sort() #对数组进行排序
-		i = 0
-		j = len(nums) - 1
-		while(i < j): #分别从首尾向中间遍历进行查找
-			if nums[i] + nums[j] == target:
-				break
-			if nums[i] + nums[j] < target:
-				i += 1
-			if nums[i] + nums[j] > target:
-				j -= 1
+        nums.sort() #对数组进行排序
+        i = 0
+        j = len(nums) - 1
+        while(i < j): #分别从首尾向中间遍历进行查找
+            if nums[i] + nums[j] == target:
+                break
+            if nums[i] + nums[j] < target:
+                i += 1
+            if nums[i] + nums[j] > target:
+                j -= 1
 
-		#根据新的下标找到对应的原始下标，即所求。
-		if dic[nums[i]][0] < dic[nums[j]][-1]:
-			index1 = dic[nums[i]][0]
-			index2 = dic[nums[j]][-1]
-		else:
-			index1 = dic[nums[j]][-1]
-			index2 = dic[nums[i]][0]
-		result = [index1 + 1, index2 + 1]
-		return result
-
+        #根据新的下标找到对应的原始下标，即所求。
+        index1 = min(dic[nums[i]][0], dic[nums[j]][-1])
+        index2 = max(dic[nums[i]][0], dic[nums[j]][-1])
+        return [index1, index2]
 
 #==================================================================================
 #解法二：
@@ -47,22 +39,15 @@ class Solution:
 #该思想是逐渐建立一个hash表，然后在其中查找。A+B=C,变换成A=C-B，这是一个思想的转变。
 #
 class Solution:
-	#@param {integer[]} nums
-	#@param {integer} target
-	#@return {integer[]} 
-	def twoSum(self, nums, target):
-		dic = {} #用来存储原始数组中元素与下标的映射关系，有可能一个元素对应多个下标
-		for i in range(len(nums)):
-			if dic.has_key(target - nums[i]): #在字典中查找，一旦找到，即成功。
-				index1 = dic[target - nums[i]][0]
-				index2 = i
-				result = [index1 + 1, index2 + 1]
-				return result
-			#下面与网上的版本不同，网上的虽然能提交通过，但是有错误。
-			if dic.has_key(nums[i]):
-				dic[nums[i]].append(i)
-			else:
-				dic[nums[i]] = []
-				dic[nums[i]].append(i)
-
+    #@param {integer[]} nums
+    #@param {integer} target
+    #@return {integer[]} 
+    def twoSum(self, nums, target):
+        dic = {} #用来存储原始数组中元素与下标的映射关系，有可能一个元素对应多个下标, 只存最小那个
+        for i, element in enumerate(nums):
+            if target - element in dic:
+                index1 = dic[target - element]
+                index2 = i
+                return [index1, index2]
+            dic[element] = dic.get(element, i)
 
