c3506d60b7348234336c13c9d30030a3378f6787
biwen <wangbiwen_88@qq.com>
2016-05-09 14:50:20 +0800

    add a code file.

---
+++ python/5_Longest_Palindromic_Substring.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#########################################################################
+# File Name: 5_Longest_Palindromic_Substring.py
+# Author: Wang Biwen
+# mail: wangbiwen88@126.com
+# Created Time: 2016.05.08
+#########################################################################
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+    
