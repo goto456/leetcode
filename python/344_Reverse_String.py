b7f2c394600a1c91baeffd7590df44f02f42125c
biwen <wangbiwen_88@qq.com>
2016-05-10 10:10:58 +0800

    finish the 338th problem.

---
+++ python/344_Reverse_String.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#########################################################################
+# File Name: 344_Reverse_String.py
+# Author: Wang Biwen
+# mail: wangbiwen88@126.com
+# Created Time: 2016.05.08
+#########################################################################
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
