3c7c465e8178cd851f518731deb9a0c9b602db44
biwen <wangbiwen_88@qq.com>
2016-05-11 18:26:54 +0800

    add a code file.

---
+++ python/338_Counting_Bits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#########################################################################
+# File Name: 338_Counting_Bits.py
+# Author: Wang Biwen
+# mail: wangbiwen88@126.com
+# Created Time: 2016.05.08
+#########################################################################
+
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        result = []
+        for i in range(num):
+
