c686c1c0a139ef9905de82c18467ff12dd83286b
wangbiwen [王必文] <wangbiwen@xiaomi.com>
2017-03-02 20:37:18 +0800

    finish the 500th problem.

---
+++ leetcode-java/src/KeyboardRow_500.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * FileName: KeyboardRow_500.java
+ * Description: 对键盘上的字母建立一个Hash表，在同一行上的字母的哈希值相同，则可判断出结果
+ * Authors: wangbiwen
+ * Date: 17-3-2
+ */
+public class KeyboardRow_500 {
+    public String[] findWords(String[] words) {
+        String[] alphabet = {"qwertyuiop", "asdfghjkl", "zxcvbnm"};
+        Map<Character, Integer> alphabetMap = new HashMap<>();
+        for (int i = 0; i < alphabet.length; i++) {
+            for (char c : alphabet[i].toCharArray()) {
+                alphabetMap.put(c, i);
+            }
+        }
+
+        List<String> result = new LinkedList<>();
+        for (String word : words) {
+            int flag = alphabetMap.get(word.toLowerCase().charAt(0));
+            for (char c : word.toLowerCase().toCharArray()) {
+                if (flag != alphabetMap.get(c)) {
+                    flag = -1;
+                    break;
+                }
+            }
+            if (flag != -1) {
+                result.add(word);
+            }
+        }
+        return result.toArray(new String[0]);
+    }
+
+    public static void main(String[] args) {
+        KeyboardRow_500 keyboardRow = new KeyboardRow_500();
+        String[] words = {"Hello", "Alaska", "Dad", "Peace"};
+        String[] result = keyboardRow.findWords(words);
+        for (String word : result) {
+            System.out.println(word);
+        }
+    }
+}
