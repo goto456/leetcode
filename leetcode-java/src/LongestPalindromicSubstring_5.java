b385a01253228f601153d558259d49b46ac4cc1b
goto456 <goto456@126.com>
2018-01-10 08:30:41 +0800

    edit the 5th problem

--- leetcode-java/src/LongestPalindromicSubstring_5.java
+++ leetcode-java/src/LongestPalindromicSubstring_5.java
@@ -2,6 +2,9 @@
  * Created by goto456 on 2017/11/19.
  */
 public class LongestPalindromicSubstring_5 {
+    /**
+     * 解法1：中心扩展方法
+     */
     public static String longestPalindrome(String s) {
         int left = 0;
         int right = 0;
@@ -29,9 +32,42 @@ public class LongestPalindromicSubstring_5 {
         return right - left - 1;
     }
 
+    /**
+     * Manacher 算法，线性时间复杂度
+     */
+    public static String manacher(String s) {
+        // 先将原字符串构造成特殊字符串
+        char[] charArray = s.toCharArray();
+        StringBuilder sb = new StringBuilder("#");
+        for (int i = 0; i < charArray.length; i++) {
+            sb.append(charArray[i]);
+            sb.append("#");
+        }
+
+        int maxCenter = 0;
+        int maxRight = 0;
+        int[] p = new int[sb.length()];
+        for (int i = 0; i < p.length; i++) {
+            p[i] = maxRight > i ? Math.min(p[2 * maxCenter - i], maxRight - i) : i;
+
+            while (i + p[i] < sb.length() && i - p[i] >=0 && sb.charAt(i + p[i]) == sb.charAt(i - p[i])) {
+                p[i]++;
+            }
+
+            if (maxRight < i + p[i]) {
+                maxRight = i + p[i];
+                maxCenter = i;
+            }
+        }
+
+        return s.substring(2 * maxCenter - maxRight, maxRight - maxCenter);
+    }
+
     public static void main(String[] args) {
         String s = "babad";
         String result = longestPalindrome(s);
+        System.out.println(result);
+        result = manacher(s);
         System.out.println(result);
     }
 }
