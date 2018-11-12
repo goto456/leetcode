99e1f68f5090cabcb7402a9c532faa3bda5940ff
goto456 <goto456@126.com>
2018-11-12 23:16:52 +0800

    finish the 9th problem

---
+++ leetcode-java/src/_009_PalindromeNumber.java
@@ -0,0 +1,33 @@
+/**
+ * Created by goto456 on 2018/11/12.
+ */
+public class _009_PalindromeNumber {
+    public static boolean isPalindrome(int x) {
+        if (x == 0) {
+            return true;
+        }
+
+        if (x < 0 || x % 10 == 0) {
+            return false;
+        }
+
+        int reverse = 0;
+        while (x > reverse) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == reverse || x == reverse / 10;
+    }
+
+    public static void main(String[] args) {
+        int x1 = 121;
+        System.out.println(isPalindrome(x1));
+
+        int x2 = -121;
+        System.out.println(isPalindrome(x2));
+
+        int x3 = 10;
+        System.out.println(isPalindrome(x3));
+    }
+}
