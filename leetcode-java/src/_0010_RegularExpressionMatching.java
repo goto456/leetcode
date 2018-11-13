058c1ef69f1eb7eb44f3663164431155b6b7ee26
goto456 <goto456@126.com>
2018-11-13 23:02:19 +0800

    finish the 10th problem

---
+++ leetcode-java/src/_0010_RegularExpressionMatching.java
@@ -0,0 +1,19 @@
+/**
+ * Created by goto456 on 2018/11/13.
+ */
+public class _0010_RegularExpressionMatching {
+    public static boolean isMatch(String s, String p) {
+        if (p.length() == 0) {
+            return s.length() == 0;
+        }
+
+        boolean first_match = (s.length() != 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.'));
+
+        if (p.length() == 1 || p.charAt(1) != '*') {
+            return first_match && isMatch(s.substring(1), p.substring(1));
+        } else {
+            return (isMatch(s, p.substring(2)) ||
+                    (first_match && isMatch(s.substring(1), p)));
+        }
+    }
+}
