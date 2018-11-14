054a9dd7e1a73744f81d3d1fbbe76763f174f1ac
goto456 <goto456@126.com>
2018-11-14 08:27:02 +0800

    添加注释

--- leetcode-java/src/_0010_RegularExpressionMatching.java
+++ leetcode-java/src/_0010_RegularExpressionMatching.java
@@ -7,8 +7,10 @@ public class _0010_RegularExpressionMatching {
             return s.length() == 0;
         }
 
+        // 表示第一个字符是否匹配上
         boolean first_match = (s.length() != 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.'));
 
+        // 如果pattern是"x*"类型的话,那么pattern每次要两个两个的减少。否则,就是一个一个的减少
         if (p.length() == 1 || p.charAt(1) != '*') {
             return first_match && isMatch(s.substring(1), p.substring(1));
         } else {
