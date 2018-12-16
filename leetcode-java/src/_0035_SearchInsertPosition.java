6df0a61b83c6a59659f72102b491974e01a71b95
goto456 <goto456@126.com>
2018-12-16 12:04:08 +0800

    finish the 35th problem

--- leetcode-java/src/_0035_SearchInsertPosition.java
+++ leetcode-java/src/_0035_SearchInsertPosition.java
@@ -3,20 +3,18 @@
  */
 public class _0035_SearchInsertPosition {
     public static int searchInsert(int[] nums, int target) {
-        if (nums[0] >= target) {
-            return 0;
-        }
-        if (nums[nums.length - 1] < target) {
-            return nums.length;
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (i == nums.length - 1) {
-                return nums.length - 1;
-            }
-            if (nums[i] < target && nums[i + 1] >= target) {
-                return i + 1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        return nums.length;
+        return left;
     }
 }
