db4a212d72f2c54d6d981e911ebc1d903b491383
goto456 <goto456@126.com>
2020-05-16 19:40:52 +0800

    finish the 111st problem

---
+++ leetcode-java/src/_0111_MinimumDepthOfBinaryTree.java
@@ -0,0 +1,20 @@
+/**
+ * Created by goto456 on 2020/5/14.
+ */
+public class _0111_MinimumDepthOfBinaryTree {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.right == null && root.left == null) {
+            return 1;
+        }
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
