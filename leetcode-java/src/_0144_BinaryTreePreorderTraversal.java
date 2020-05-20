5b6d0bcb658502dabde56cd57908b43d64264cd9
goto456 <goto456@126.com>
2020-05-20 23:00:00 +0800

    finish the 144th problem

---
+++ leetcode-java/src/_0144_BinaryTreePreorderTraversal.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created by goto456 on 2020/5/18.
+ */
+public class _0144_BinaryTreePreorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return result;
+    }
+}
