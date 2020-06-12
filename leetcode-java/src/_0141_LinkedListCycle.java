0c6398d62b225d51e3aca6e25418163a028dc5c0
goto456 <goto456@126.com>
2020-06-12 23:17:36 +0800

    finish the 141st problem

---
+++ leetcode-java/src/_0141_LinkedListCycle.java
@@ -0,0 +1,24 @@
+/**
+ * Created by goto456 on 2020/6/2.
+ */
+public class _0141_LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            // 快指针先到达终点，则表示没有环
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            // 慢指针每次前进1步
+            slow = slow.next;
+            // 快指针每次前进2步
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
