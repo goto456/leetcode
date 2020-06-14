b4cfdf95fa5db3c40e907397b82c2a034b87039d
goto456 <goto456@126.com>
2020-06-14 23:45:32 +0800

    finish the 1300th problem

---
+++ leetcode-java/src/_1300_SumOfMutatedArrayClosestToTarget.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+/**
+ * Created by goto456 on 2020/6/14.
+ */
+public class _1300_SumOfMutatedArrayClosestToTarget {
+    public int findBestValue(int[] arr, int target) {
+        // 先排序
+        Arrays.sort(arr);
+        int n = arr.length;
+        int[] prefix = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] + arr[i - 1];
+        }
+
+        int l = 0;
+        int r = arr[n - 1];
+        int ans = -1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            int index = Arrays.binarySearch(arr, mid);
+            if (index < 0) {
+                index = -index - 1;
+            }
+            int cur = prefix[index] + (n - index) * mid;
+            if (cur <= target) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        int chooseSmall = check(arr, ans);
+        int chooseBig = check(arr, ans + 1);
+        return Math.abs(chooseSmall - target) <= Math.abs(chooseBig - target) ? ans : ans + 1;
+    }
+
+    private int check(int[] arr, int x) {
+        int ret = 0;
+        for (int num : arr) {
+            ret += Math.min(num, x);
+        }
+        return ret;
+    }
+}
