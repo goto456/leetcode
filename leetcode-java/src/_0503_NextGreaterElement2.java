232e6a09167d17efbc0b86729376d96ce27a64a9
goto456 <goto456@126.com>
2020-07-05 23:40:12 +0800

    start solving the 503rd problem

---
+++ leetcode-java/src/_0503_NextGreaterElement2.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+/**
+ * 单调栈解此题
+ * Created by goto456 on 2020/7/5.
+ */
+public class _0503_NextGreaterElement2 {
+    public int[] nextGreaterElements(int[] nums) {
+        int[] result = new int[nums.length];
+        Arrays.fill(result, -1);
+        Stack<Integer> stack = new Stack<>();
+
+
+    }
+}
