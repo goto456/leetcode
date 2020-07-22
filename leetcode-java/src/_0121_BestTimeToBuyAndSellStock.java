e3547d4a99a7839f72c768030e030082022c780f
goto456 <goto456@126.com>
2020-07-22 23:32:11 +0800

    finish the 121st problem

---
+++ leetcode-java/src/_0121_BestTimeToBuyAndSellStock.java
@@ -0,0 +1,17 @@
+/**
+ * Created by goto456 on 2020/7/22.
+ */
+public class _0121_BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
