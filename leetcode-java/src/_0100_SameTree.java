19d8cdecf7b91edfe0785c07dd861f974dafa567
goto456 <goto456@126.com>
2020-07-24 23:12:05 +0800

    finish the 100th problem

---
+++ leetcode-java/src/_0100_SameTree.java
@@ -0,0 +1,20 @@
+/**
+ * Created by goto456 on 2020/7/24.
+ */
+public class _0100_SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
