3683adf122f24107c275f78b190d451c0c3e41e9
goto456 <goto456@126.com>
2023-08-05 23:18:57 +0800

    finish the 141st problem.

--- leetcode-java/src/_0141_LinkedListCycle.java
+++ leetcode-java/src/_0141_LinkedListCycle.java
@@ -21,4 +21,18 @@ public class _0141_LinkedListCycle {
         }
         return true;
     }
+
+    // 解法2，更简单易懂
+    public boolean hasCycle2(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
